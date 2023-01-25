7abbbe29a020f1c9a20c6b959fb7e395d0d61a70
usama <uusman004@gmail.com>
2023-01-26 02:15:35 +0500

    FEAT:Implement Credentials Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authOptions = {
 
@@ -8,6 +9,31 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
+
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Enter Your DB Logic Here 
+
+                const user = {
+                    name: "Usama Usman",
+                    image: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
+                    email: "uusman004@gmail.com"
+                }
+
+                if (credentials.username === "Usama" && credentials.password === "mani123") {
+                    return user
+                }
+
+                else {
+                    return null
+                }
+            }
+        })
     ],
 }
 
